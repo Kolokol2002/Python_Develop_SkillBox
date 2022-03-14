3962dcbed86ff6f1371500139ff528f87dd6a7d4
Maks <maks.karalash@gmail.com>
2022-03-14 21:18:51 +0200

    Errors02 DONE - lesson10

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,54 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+import random
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+class IamGodError(Exception):
+    pass
+
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+carma_count = 0
+
+list_excepts = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError,
+                SuicideError]
+
+def one_day():
+    global carma_count
+    carma = random.randint(1, 7)
+    carma_count += carma
+    excepts = random.randint(1, 13)
+    if excepts == 1:
+        random_excepts = random.choice(list_excepts)
+
+        raise random_excepts(random_excepts)
+
+while carma_count <= ENLIGHTENMENT_CARMA_LEVEL:
+    try:
+        one_day()
+    except Exception as exc :
+        print(f'Оппа, ошибочка - {exc}')
 
 # https://goo.gl/JnsDqu
