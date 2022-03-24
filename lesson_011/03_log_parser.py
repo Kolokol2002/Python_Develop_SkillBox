6bd45a2e9157bd3b69e3100935c65476802190a1
Maks <maks.karalash@gmail.com>
2022-03-24 15:12:16 +0200

    03 DONE - lesson11

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,25 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+def parser(file):
+    res_list = {}
+    with open(file, 'r', encoding='utf8') as text:
+        for i in text:
+            d = i.translate(str.maketrans('', '', '[]\n', ))
+            date, time, text = d.split(' ')
+            if 'NOK' == text:
+                time = time.split(':')[:-1]
+                time_res = f'{time[0]}:{time[1]}'
+                if f'{date} {time_res}' not in res_list:
+                    res_list[f'{date} {time_res}'] = 1
+                else:
+                    res_list[f'{date} {time_res}'] += 1
+
+    for datatime, count in res_list.items():
+        yield f'{datatime}: {count}'
+
+
+grouped_events = parser('events.txt')
+for group_time in grouped_events:
+    print(group_time)
+    # print(f'[{group_time}] {event_count}')
