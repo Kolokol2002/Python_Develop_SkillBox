9adb631952f0997f0a13f75bf08c5d53c375cd43
Maks <maks.karalash@gmail.com>
2022-03-24 18:34:25 +0200

    04 DONE - lesson11

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -7,19 +7,38 @@
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
+lines = [
+    'Ярослав bxh@ya.ru 600',
+    'Земфира tslzp@mail.ru 52',
+    'Тролль nsocnzas.mail.ru 82',
+    'Джигурда wqxq@gmail.com 29',
+    'Земфира 86',
+    'Равшан wmsuuzsxi@mail.ru 35',
+]
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+def log_errors(param):
+    def log_errors2(func):
+        def logger(*args, **kwargs):
+            with open(param, 'a', encoding='utf8') as file:
+                try:
+                    func(*args, **kwargs)
+                except Exception as exc:
+                    file.write(f"Ім'я функції - {func.__name__}, "
+                              f"Параметри функції - {str(*args)}, "
+                              f"Тип помилки - {exc.__class__},"
+                              f"Текс помилки - {exc}.\n")
+        return logger
+
+    return log_errors2
 
 
 # Проверить работу на следующих функциях
-@log_errors
+@log_errors(param='function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors(param='function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -30,19 +49,10 @@ def check_line(line):
         raise ValueError('Age not in 10..99 range')
 
 
-lines = [
-    'Ярослав bxh@ya.ru 600',
-    'Земфира tslzp@mail.ru 52',
-    'Тролль nsocnzas.mail.ru 82',
-    'Джигурда wqxq@gmail.com 29',
-    'Земфира 86',
-    'Равшан wmsuuzsxi@mail.ru 35',
-]
+
 for line in lines:
-    try:
-        check_line(line)
-    except Exception as exc:
-        print(f'Invalid format: {exc}')
+    check_line(line)
+
 perky(param=42)
 
 
---
+++ lesson_011/python_snippets/probe.py
@@ -0,0 +1,24 @@
+import time
+
+
+def time_track(func):
+    def surrogate(*args, **kwargs):
+        started_at = time.time()
+
+        result = func(*args, **kwargs)
+
+        ended_at = time.time()
+        elapsed = round(ended_at - started_at, 4)
+        print(f'Функция работала {elapsed} секунд(ы)')
+        return result
+    return surrogate
+
+
+@time_track
+def digits(*args):
+    total = 1
+    for number in args:
+        total *= number ** 5000
+    return len(str(total))
+
+print(digits(1123, 21312, 21312))
