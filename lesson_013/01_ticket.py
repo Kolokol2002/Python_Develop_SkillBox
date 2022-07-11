349cb665913c09502c1a9cc7d8ca91a5994b3c7d
Maks <maks.karalash@gmail.com>
2022-07-11 18:15:47 +0300

    DONE - lesson13

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from PIL import Image, ImageFont, ImageDraw, ImageColor
+
+import argparse
+
 
 # Заполнить все поля в билете на самолет.
 # Создать функцию, принимающую параметры: ФИО, откуда, куда, дата вылета,
@@ -8,9 +12,24 @@
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
-def make_ticket(fio, from_, to, date):
+
+def make_ticket(fio, from_, to, date, save_to):
     # TODO здесь ваш код
-    pass
+    im = Image.open('./images/ticket_template.png')
+
+    font = ImageFont.truetype('Roboto-Black.ttf', size=15)
+
+    draw_text = ImageDraw.Draw(im)
+    draw_text.text((45, 122), fio, font=font, fill=ImageColor.colormap['black'])
+    draw_text.text((45, 190), from_, font=font, fill=ImageColor.colormap['black'])
+    draw_text.text((45, 260), to, font=font, fill=ImageColor.colormap['black'])
+    draw_text.text((285, 260), date, font=font, fill=ImageColor.colormap['black'])
+    if save_to == None:
+        im.save('ticket_airplane.png')
+    else:
+        im.save(save_to)
+
+
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля agrparse.
@@ -21,3 +40,15 @@ def make_ticket(fio, from_, to, date):
 #   --date - обязательный, когда летим.
 #   --save_to - необязательный, путь для сохранения заполненнего билета.
 # и заполнять билет.
+
+parser = argparse.ArgumentParser(description='Ping script')
+
+parser.add_argument('--fio', dest='fio', type=str)
+parser.add_argument('--from', dest='from_', type=str)
+parser.add_argument('--to', dest='to', type=str)
+parser.add_argument('--date', dest='date', type=str)
+parser.add_argument('--save_to', dest='save_to', type=str)
+
+args = parser.parse_args()
+
+make_ticket(fio=args.fio, from_=args.from_, to=args.to, date=args.date, save_to=args.save_to)
